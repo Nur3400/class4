962cfd783132d88c26f186aeaed0f2ea6956da51
“Nur3400” <mathnurislam@gmail.com>
2021-05-01 14:29:43 +0600

    nur

---
+++ 3 angle.py
@@ -0,0 +1,8 @@
+print("Wellcome to python programing")
+print("...............................")
+a=int(input("Enter first angle"))
+b=int(input("Enter second angle"))
+c=int(input("Enter third angle"))
+d=a+b+c 
+print(d)
+    
---
+++ Monkey math.py
@@ -0,0 +1,7 @@
+print("Wellcome to python Pragmaming")
+print("..................")
+a=int(input("Joto step Upore uthe"))
+b=int(input("Joto step niche name"))
+c=int(input("Final joto step a joto niche nambe"))
+d=(b*c)/a
+print("Total niche nambe",d,)
