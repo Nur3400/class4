90a41c562b89ca4b64ca4ca1e62ce2a957fda410
“Nur3400” <mathnurislam@gmail.com>
2021-05-01 13:24:20 +0600

    math

---
+++ math.py
@@ -0,0 +1,8 @@
+def ex(a,b):
+    return (a*(10**b))
+a=float(input("Enter the value of A"))
+b=float(input("Enter the value of B"))
+c=int(input("Enter the value of c"))
+d=c*60
+s= (ex(a,b))*d
+print(s)
